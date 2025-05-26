0c744f82f704bc5f57a07c6651b27d4f0f98808c
francesco-saponaro <francesco.s@zumbat.it>
2025-05-26 13:07:26 +0200

    feat: update puzzles

--- src/routes/constants/PUZZLE.js
+++ src/routes/constants/PUZZLE.js
@@ -8,6 +8,11 @@ import SpringSnack from "@assets/images/SpringSnack.webp";
 import Jamboree from "@assets/images/Jamboree.webp";
 import PelosettiBaffuti from "@assets/images/PelosettiBaffuti.webp";
 import CucinaDiOsaka from "@assets/images/CucinaDiOsaka.webp";
+import TokyoNights from "@assets/images/TokyoNights.webp";
+import SogniDiCarta from "@assets/images/SogniDiCarta.webp";
+import LeAcqueDiFuji from "@assets/images/LeAcqueDiFuji.webp";
+import IGuardianiDiKyoto from "@assets/images/IGuardianiDiKyoto.webp";
+import IncontroAlGranCanale from "@assets/images/IncontroAlGranCanale.webp";
 
 const PuzzleList = [
   {
@@ -19,43 +24,43 @@ const PuzzleList = [
     orientation: "portrait",
     soldOut: false,
   },
-  {
-    img: SanGhostino,
-    title: "San Ghostino",
-    description: "Componi il puzzle del borgo infestato da fantasmi.",
-    link: "https://stopuzzle.com/collections/solo-puzzle/products/san-gostino",
-    id: "san-ghostino",
-    orientation: "landscape",
-    soldOut: false,
-  },
-  {
-    img: PazzoPomeriggio,
-    title: "Pazzo pomeriggio",
-    description: "Componi il puzzle più caotico e divertente.",
-    link: "https://stopuzzle.com/collections/solo-puzzle/products/pazzo-pomeriggio",
-    id: "pazzo-pomeriggio",
-    orientation: "portrait",
-    soldOut: false,
-  },
-  {
-    img: IDolciSegretiDelNatale,
-    title: "I dolci segreti del Natale",
-    description: "Il puzzle natalizio più dolce che tu possa mai comporre.",
-    link: "https://stopuzzle.com/collections/solo-puzzle/products/i-dolci-segreti-del-natale-1000-pezzi",
-    id: "i-dolci-segreti-del-natale",
-    orientation: "landscape",
-    soldOut: false,
-  },
-  {
-    img: WhatWillBe,
-    title: "What will be",
-    description:
-      "La migliore illustrazione che ti invita a goderti i tuoi momenti.",
-    link: "https://stopuzzle.com/collections/solo-puzzle/products/what-will-be-will-be",
-    id: "what-will-be",
-    orientation: "portrait",
-    soldOut: false,
-  },
+  // {
+  //   img: SanGhostino,
+  //   title: "San Ghostino",
+  //   description: "Componi il puzzle del borgo infestato da fantasmi.",
+  //   link: "https://stopuzzle.com/collections/solo-puzzle/products/san-gostino",
+  //   id: "san-ghostino",
+  //   orientation: "landscape",
+  //   soldOut: false,
+  // },
+  // {
+  //   img: PazzoPomeriggio,
+  //   title: "Pazzo pomeriggio",
+  //   description: "Componi il puzzle più caotico e divertente.",
+  //   link: "https://stopuzzle.com/collections/solo-puzzle/products/pazzo-pomeriggio",
+  //   id: "pazzo-pomeriggio",
+  //   orientation: "portrait",
+  //   soldOut: false,
+  // },
+  // {
+  //   img: IDolciSegretiDelNatale,
+  //   title: "I dolci segreti del Natale",
+  //   description: "Il puzzle natalizio più dolce che tu possa mai comporre.",
+  //   link: "https://stopuzzle.com/collections/solo-puzzle/products/i-dolci-segreti-del-natale-1000-pezzi",
+  //   id: "i-dolci-segreti-del-natale",
+  //   orientation: "landscape",
+  //   soldOut: false,
+  // },
+  // {
+  //   img: WhatWillBe,
+  //   title: "What will be",
+  //   description:
+  //     "La migliore illustrazione che ti invita a goderti i tuoi momenti.",
+  //   link: "https://stopuzzle.com/collections/solo-puzzle/products/what-will-be-will-be",
+  //   id: "what-will-be",
+  //   orientation: "portrait",
+  //   soldOut: false,
+  // },
   {
     img: MrGonfiabile,
     title: "Mr gonfiabile",
@@ -65,15 +70,15 @@ const PuzzleList = [
     orientation: "portrait",
     soldOut: false,
   },
-  {
-    img: SpringSnack,
-    title: "Spring snack",
-    description: "Puzzle paesaggistico per evadere dalla realtà quotidiana.",
-    link: "https://stopuzzle.com/collections/solo-puzzle/products/spring-snack-500-pezzi",
-    id: "spring-snack",
-    orientation: "portrait",
-    soldOut: false,
-  },
+  // {
+  //   img: SpringSnack,
+  //   title: "Spring snack",
+  //   description: "Puzzle paesaggistico per evadere dalla realtà quotidiana.",
+  //   link: "https://stopuzzle.com/collections/solo-puzzle/products/spring-snack-500-pezzi",
+  //   id: "spring-snack",
+  //   orientation: "portrait",
+  //   soldOut: false,
+  // },
   {
     img: Jamboree,
     title: "Jamboree",
@@ -99,6 +104,54 @@ const PuzzleList = [
     link: "https://stopuzzle.com/collections/solo-puzzle/products/la-cucina-di-osaka",
     id: "la-cucina-di-osaka",
     orientation: "landscape",
+    soldOut: false,
+  },
+  {
+    img: TokyoNights,
+    title: "Tokyo Nights",
+    description: "Un puzzle che ti farà sognare le notti di Tokyo.",
+    link: "https://stopuzzle.com/collections/solo-puzzle/products/tokyo-girls-puzzle-500-pezzi",
+    id: "tokyo-nights",
+    orientation: "portrait",
+    soldOut: false,
+  },
+  {
+    img: SogniDiCarta,
+    title: "Sogni di carta",
+    description:
+      "Un puzzle che celebra la bellezza della carta e della creatività.",
+    link: "https://stopuzzle.com/collections/solo-puzzle/products/sogni-di-carta-1000-pezzi",
+    id: "sogni-di-carta",
+    orientation: "portrait",
+    soldOut: false,
+  },
+  {
+    img: LeAcqueDiFuji,
+    title: "Le acque di Fuji",
+    description: "Un puzzle che cattura la serenità delle acque giapponesi.",
+    link: "https://stopuzzle.com/collections/solo-puzzle/products/le-acque-di-fuji-puzzle-1000-pezzi",
+    id: "le-acque-di-fuji",
+    orientation: "landscape",
+    soldOut: false,
+  },
+  {
+    img: IGuardianiDiKyoto,
+    title: "I guardiani di Kyoto",
+    description:
+      "Un puzzle che ti trasporta nel cuore della cultura giapponese.",
+    link: "https://stopuzzle.com/collections/solo-puzzle/products/i-guardiani-di-kyoto-puzzle-1000-pezzi",
+    id: "i-guardiani-di-kyoto",
+    orientation: "portrait",
+    soldOut: false,
+  },
+  {
+    img: IncontroAlGranCanale,
+    title: "Incontro al Gran Canale",
+    description:
+      "Un puzzle che celebra la bellezza dei canali veneziani e la loro storia.",
+    link: "https://stopuzzle.com/collections/solo-puzzle/products/incontro-al-canal-grande-puzzle-500-pezzi",
+    id: "incontro-al-gran-canale",
+    orientation: "portrait",
     soldOut: false,
   },
 ];
